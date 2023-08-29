acb571db237978c3c54769a4b33d589d58925ec4
Hamza Rehman <hamzarh911@gmail.com>
2023-08-29 10:01:31 +0500

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
 import 'package:firebase_remote_config/firebase_remote_config.dart';
-import 'package:firebase_remote_config_demo/home_screen.dart';
+import 'package:firebase_remote_config_demo/remote_config/firebase_remote_config_service.dart';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,13 +22,13 @@ void main() async {
     await remoteConfig
         .setConfigSettings(RemoteConfigSettings(
       fetchTimeout: const Duration(minutes: 1),
-      minimumFetchInterval: const Duration(hours: 1),
+      minimumFetchInterval: const Duration(seconds: 1),
     ))
         .whenComplete(() async {
       await remoteConfig.fetch().whenComplete(() async {
         await remoteConfig.activate().whenComplete(() async {
-          prefs!.setString(
-              'post_data', jsonEncode(remoteConfig.getString('post_data')));
+          prefs!.setString('post_data', remoteConfig.getString('post_data'));
+          FirebaseRemoteConfigService().loadData();
         });
       });
     });
@@ -39,9 +40,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         home: Scaffold(
-      body: Text(''),
+      body: SafeArea(
+        child: ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            itemCount: FirebaseRemoteConfigService.remoteModel.length,
+            itemBuilder: ((context, index) {
+            
+              return Text(
+                  FirebaseRemoteConfigService.remoteModel[index].id.toString());
+            })),
+      ),
     ));
   }
 }
---
+++ lib/remote_config/firebase_remote_config_service.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:firebase_remote_config_demo/main.dart';
+import 'package:firebase_remote_config_demo/model/remote_config_model.dart';
+
+class FirebaseRemoteConfigService {
+  static List remoteModel = <RemoteConfigModel>[];
+  void loadData() {
+    if (prefs!.containsKey('post_data')) {
+      var data = prefs!.getString('post_data')!;
+      var decodedData = jsonDecode(data);
+
+      remoteModel.clear();
+      for (var element in decodedData) {
+        remoteModel.add(
+          RemoteConfigModel(
+              userId: element['userId'],
+              id: element['id'],
+              title: element['title'],
+              body: element['body']),
+        );
+      }
+    }
+  }
+}
